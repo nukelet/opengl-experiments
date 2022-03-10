82c48731900d3f2c0e85c1cc1758e53d4d6fe9e8
nukelet <nukelet@protonmail.com>
2022-03-09 23:25:49 -0300

    Add a shader class

--- include/utils/shader.hpp
+++ include/utils/shader.hpp
@@ -1,11 +1,27 @@
 #ifndef _SHADER_HPP
 #define _SHADER_HPP
 
+#include <string>
 #include <glad/glad.h>
 
-class Shader {
+class ShaderProgram {
     private:
+
+    struct Shader {
+        GLuint id;
+        std::string filepath;
+        std::string src;
+    }; 
+
+    public:
     GLuint id;
+
+    void compile_shader(Shader shader);
+    void print_shader_info_log(Shader shader);
+    void print_program_info_log();
+
+    Shader vshader;
+    Shader fshader;
 
     public:
     
@@ -15,12 +31,13 @@ class Shader {
      * @param vshader_path  path to the vertex shader
      * @param fshader_path  path to the fragment shader 
      */
-    Shader(const char *vshader_path, const char *fshader_path);
+    ShaderProgram(const char *vshader_path, const char *fshader_path);
 
     /**
      * @brief Use the current shader
      */
     void use();
+    void setFloat(const char *name, float value);
 };
 
 #endif
--- src/utils/shader.cpp
+++ src/utils/shader.cpp
@@ -1,7 +1,100 @@
 #include <utils/shader.hpp>
 #include <fstream>
+#include <iostream>
+#include <sstream>
 
-Shader::Shader(const char *vshader_path, const char *fshader_path)
+void ShaderProgram::print_shader_info_log(Shader shader)
 {
+    static char log_buffer[1024];
+    glGetShaderInfoLog(shader.id, sizeof(log_buffer), NULL, log_buffer);
+    printf("Shader (%s) compilation failed:\n", shader.filepath.c_str());
+    printf("%s", log_buffer);
+}
+
+void ShaderProgram::print_program_info_log()
+{
+    static char log_buffer[1024];
+    glGetProgramInfoLog(id, sizeof(log_buffer), NULL, log_buffer);
+    printf("Program linking failed:\n");
+    printf("%s", log_buffer);
+}
+
+void ShaderProgram::compile_shader(Shader shader)
+{
+    GLint success;
+    const char *src = shader.src.c_str();
+    glShaderSource(shader.id, 1, &src, NULL);
+    glCompileShader(shader.id);
+    glGetShaderiv(shader.id, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        print_shader_info_log(shader);
+    }
+}
+
+ShaderProgram::ShaderProgram(const char *vshader_path, const char *fshader_path)
+{
+    // ugly hack to get the full path to the src/shaders folder
+    // SHADER_DIR is exported by CMake as a preprocessor env variable
+    #ifdef SHADER_DIR 
+    char path_buf[1024];
+    snprintf(path_buf, 1024, "%s/%s", SHADER_DIR, vshader_path);
+    std::ifstream vshader_file(path_buf);
+    snprintf(path_buf, 1024, "%s/%s", SHADER_DIR, fshader_path);
+    std::ifstream fshader_file(path_buf);
+    #else
     std::ifstream vshader_file(vshader_path);
+    std::ifstream fshader_file(fshader_path);
+    #endif
+
+    if (vshader_file.fail()) {
+        printf("Failed to open shader: %s\n", vshader_path);
+        return;
+    } else if (fshader_file.fail()) {
+        printf("Failed to open shader: %s\n", fshader_path);
+        return;
+    }
+
+    std::stringstream vshader_buf, fshader_buf;
+    vshader_buf << vshader_file.rdbuf();
+    fshader_buf << fshader_file.rdbuf();
+
+    vshader.id = glCreateShader(GL_VERTEX_SHADER);
+    vshader.filepath = std::string(vshader_path);
+    vshader.src = vshader_buf.str();
+
+    fshader.id = glCreateShader(GL_FRAGMENT_SHADER);
+    fshader.filepath = std::string(fshader_path);
+    fshader.src = fshader_buf.str();
+
+    compile_shader(vshader);
+    compile_shader(fshader);
+    
+    GLint success;
+
+    id = glCreateProgram();
+    glAttachShader(id, vshader.id);
+    glAttachShader(id, fshader.id);
+    glLinkProgram(id);
+    glGetProgramiv(id, GL_LINK_STATUS, &success);
+    if (!success) {
+        print_program_info_log();
+    }
+
+    glDeleteShader(vshader.id);
+    glDeleteShader(fshader.id);
+}
+
+void ShaderProgram::use()
+{
+    glUseProgram(id);
+}
+
+void ShaderProgram::setFloat(const char *name, float value)
+{
+    GLint location = glGetUniformLocation(id, name);
+    if (location == -1) {
+        // printf("Error setting value for \"%s\": uniform not found\n", name);
+        return;
+    }
+    glUniform1f(location, value);
 }
